f6fb4d650cde108a9784b28dcde379cf5d376a11
z1shivam <shivamk.json@gmail.com>
2025-02-17 04:16:38 +0530

    built register and login function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -10,8 +10,10 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type HelloResponse struct {
@@ -48,6 +50,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Get("/", router.HomeHandler)
 	r.Post("/register", router.RegisterHandler)
+	r.Post("/login", router.LoginHandler)
 
 	log.Println("Starting server on " + os.Getenv("LOCALHOST_URL"))
 	if os.Getenv("MODE") == "DEV" {
@@ -82,6 +85,16 @@ func (router *Router) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func VerifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 type RegisterUserInput struct {
 	Username string `json:"username"`
 	FullName string `json:"fullName"`
@@ -104,13 +117,99 @@ func (router *Router) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	collection := router.Client.Database(os.Getenv("MONGO_DB")).Collection("users")
-	_, err := collection.InsertOne(context.TODO(), user)
-	if err != nil {
-		http.Error(w, "Failed to register user", http.StatusInternalServerError)
+
+	var existingUser RegisterUserInput
+	err := collection.FindOne(context.TODO(), bson.D{{Key: "username", Value: user.Username}}).
+		Decode(&existingUser)
+
+	if existingUser.Username != "" {
+		http.Error(w, "user already exists", http.StatusBadRequest)
 		return
 	}
 
+	if hashedPassword, err := HashPassword(user.Password); err == nil {
+		user.Password = hashedPassword
+	}
+
+	if err == mongo.ErrNoDocuments {
+		_, err := collection.InsertOne(context.TODO(), user)
+		if err != nil {
+			http.Error(w, "Failed to register user", http.StatusInternalServerError)
+			return
+		}
+	}
+
 	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+}
+
+type LoginRes struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	User    *struct {
+		Username   string `json:"username"`
+		Email      string `json:"email"`
+		Logintoken string `json:"loginToken"`
+	}
+}
+
+type UserToBeLoggedIn struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (router *Router) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var gotUser UserToBeLoggedIn
+	if err := json.NewDecoder(r.Body).Decode(&gotUser); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	loginRes := &LoginRes{
+		Success: false,
+		Message: "Can't login user",
+		User:    nil,
+	}
+
+	var user RegisterUserInput
+	collection := router.Client.Database(os.Getenv("MONGO_DB")).Collection("users")
+	err := collection.FindOne(context.TODO(), bson.D{{Key: "username", Value: gotUser.Username}}).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	if !VerifyPassword(gotUser.Password, user.Password) {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	loginToken := "some-login-token"
+
+	loginRes.Success = true
+	loginRes.Message = "User logged in successfully"
+	loginRes.User = &struct {
+		Username   string `json:"username"`
+		Email      string `json:"email"`
+		Logintoken string `json:"loginToken"`
+	}{
+		Username:   user.Username,
+		Email:      user.Email,
+		Logintoken: loginToken,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Custom-Header", "custom-value")
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "session_token",
+		Value: loginToken,
+		Path:  "/",
+	})
+
+	if err := json.NewEncoder(w).Encode(loginRes); err != nil {
 		http.Error(w, "Internal error", http.StatusInternalServerError)
 		return
 	}
--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/z1shivam/auth-mongo-go
 go 1.23.5
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
